5ccba85e0328421ceb82d9f518b6ecea2f9a1a2b
Mehdi Ouhab <ouhab.me@gmail.com>
2024-06-17 09:58:28 +0200

    Start

--- index.js
+++ index.js
@@ -4,8 +4,34 @@ const app = express();
 const port = 5001;
 
 
+
 app.listen(port, () => {
     console.info(`Server is listening on port ${port}`)
 }).on("error", (err) => {
     console.error(`Error": ${err.message}`)
 })
+
+
+
+const welcomeMovies = (req, res) => {
+    res.send("Welcome to my favourite movie list")
+}
+
+const getMovies = (req, res) => {
+    res.status(200).json(movies);
+}
+
+const searchMovie = (req, res) => {
+    const found = movies.find((movie) => movie.id == req.params.id);
+    if(found){
+        res.status(200).json(found)
+    }
+    else{
+        res.status(404).send("Not found");
+    }
+}
+
+
+app.get("/api/movies/:id", searchMovie);
+app.get("/api/movies", getMovies);
+app.get("/", welcomeMovies);
