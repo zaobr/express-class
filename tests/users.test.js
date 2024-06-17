2560f858893510d40e2a5f43471b1cdb5782c809
Mehdi Ouhab <ouhab.me@gmail.com>
2024-06-17 17:45:17 +0200

    quest_02.1

--- tests/users.test.js
+++ tests/users.test.js
@@ -1,6 +1,8 @@
 const request = require("supertest");
 const app = require("../src/app");
 const database = require("../database");
+const crypto = require("node:crypto");
+
 afterAll(() => database.end());
 
 describe("GET /api/users", () => {
@@ -26,5 +28,51 @@ describe("GET /api/users/:id", () => {
     const response = await request(app).get("/api/users/0");
 
     expect(response.status).toEqual(404);
+  });
+});
+
+describe("POST /api/users", () => {
+  it("should return created user", async () => {
+    const newUser = {
+      firstname: "Edward",
+      lastname: "Lopez",
+      email: `${crypto.randomUUID()}@wild.co`,
+      city: "Tokyo",
+      language: "Japanese",
+    };
+    const response = await request(app).post("/api/users").send(newUser);
+
+    expect(response.status).toEqual(201);
+    expect(response.body).toHaveProperty("id");
+    expect(typeof response.body.id).toBe("number");
+
+    const [result] = await database.query(
+      "SELECT * FROM users WHERE id=?",
+      response.body.id
+    );
+
+    const [userInDatabase] = result;
+
+    expect(userInDatabase).toHaveProperty("id");
+    expect(userInDatabase).toHaveProperty("firstname");
+    expect(userInDatabase).toHaveProperty("lastname");
+    expect(userInDatabase).toHaveProperty("email");
+    expect(userInDatabase).toHaveProperty("city");
+    expect(userInDatabase).toHaveProperty("language");
+    expect(userInDatabase.firstname).toStrictEqual(newUser.firstname);
+    expect(userInDatabase.lastname).toStrictEqual(newUser.lastname);
+    expect(userInDatabase.email).toStrictEqual(newUser.email);
+    expect(userInDatabase.city).toStrictEqual(newUser.city);
+    expect(userInDatabase.language).toStrictEqual(newUser.language);
+  });
+
+  it("should return an error", async () => {
+    const userWithMissingProps = { firstname: "Josh" };
+
+    const response = await request(app)
+      .post("/api/users")
+      .send(userWithMissingProps);
+
+    expect(response.status).toEqual(500);
   });
 });
